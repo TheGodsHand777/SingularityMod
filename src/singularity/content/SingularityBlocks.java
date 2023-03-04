b7466bb9fc465999fce2bbbd36a2c6a29c3ef3de
Despat <moyaposchta777666@gmail.com>
2023-03-04 19:39:11 +0100

    bra

--- src/singularity/content/SingularityBlocks.java
+++ src/singularity/content/SingularityBlocks.java
@@ -1,5 +1,6 @@
 package singularity.content;
 
+import arc.graphics.Color;
 import mindustry.entities.bullet.*;
 import mindustry.world.Block;
 import mindustry.world.blocks.defense.turrets.*;
@@ -23,26 +24,15 @@ public class SingularityBlocks {
         miracle = new ItemTurret("miracle"){{
             requirements(Category.turret, with(Items.copper, 100, Items.graphite, 80, Items.titanium, 50));
             ammo(
-                Items.copper,  new BasicBulletType(2.5f, 11) {{
-                    width = 7f;
-                    height = 9f;
-                    lifetime = 60f;
-                    ammoMultiplier = 2;
-                }},
-                Items.graphite, new ArtilleryBulletType(3f, 20) {{
-                    knockback = 0.8f;
-                    reloadMultiplier = 0.5f;
-                    lifetime = 80f;
-                    width = height = 11f;
-                    collidesTiles = false;
-                    splashDamageRadius = 25f * 0.75f;
-                    splashDamage = 33f;
-                }},
                 Items.blastCompound, new MissileBulletType(3.7f, 10) {{
                     reloadMultiplier = 0.3f;
                     width = 16f;
                     height = 16f;
                     shrinkY = 0f;
+                    lightningDamage = 10;
+                    lightning = 2;
+                    lightningLength = 10;
+                    lightningColor = red;
                     splashDamageRadius = 30f;
                     splashDamage = 30f * 1.5f;
                     ammoMultiplier = 5f;
@@ -51,23 +41,8 @@ public class SingularityBlocks {
 
                     status = StatusEffects.blasted;
                     statusDuration = 60f;
-                }},
-                Items.silicon, new BasicBulletType(3f, 15, "bullet") {{
-                    width = 7f;
-                    height = 9f;
-                    homingPower = 0.1f;
-                    reloadMultiplier = 1.5f;
-                    ammoMultiplier = 5;
-                    lifetime = 60f;
-                }},
-                Items.thorium, new BasicBulletType(4f, 29, "bullet") {{
-                    width = 10f;
-                    height = 13f;
-                    shootEffect = Fx.shootBig;
-                    smokeEffect = Fx.shootBigSmoke;
-                    ammoMultiplier = 4;
-                    lifetime = 60f;
                 }}
+
             );
 
             size = 2;
